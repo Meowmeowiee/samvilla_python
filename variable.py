01fab76999eac62d227392e33a91e0cfec37881f
unknown <Meowmeowiee>
2025-02-10 11:32:23 +0800

    Name

--- variable.py
+++ variable.py
@@ -1,10 +1,2 @@
-x = 4
-y = 5
-
-name = "Abel"
-father_name = "Gideon"
-age = 19
-
-is_student = True
-
-print(name + " " + father_name + " " + str(age) + "years old")
+name = input("Enter your name: ")
+print("Hello " + name)
