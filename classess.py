5012324e4850c464e1b12734b67cb00590e3976d
unknown <Meowmeowiee>
2025-02-10 21:20:45 +0800

    class

--- classess.py
+++ classess.py
@@ -1,2 +1,24 @@
-def add(x, y):
-    return
+class Animal:
+    def __int__(self, name):
+        self.name = name
+
+    def eat(self):
+        print("Eating........")
+        def breathe(self):
+            print("Breathing....")
+
+class Dog(Animal):
+    def sound(self):
+        print("Barking...")
+
+class Cat(Animal):
+    def sound(self):
+        print("Meow...")
+
+dog1 = Dog("Jack")
+dog1.eat()
+dog1.sound()
+
+cat1 = Cat("cat")
+cat1.eat()
+cat1.sound()
