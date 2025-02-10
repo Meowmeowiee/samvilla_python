f52b0028ed70cb6cd0d60e66765a7554f2351c4f
unknown <Meowmeowiee>
2025-02-10 20:36:06 +0800

    AlphaNum

--- containers.py
+++ containers.py
@@ -2,19 +2,27 @@ name = ["Abel", "Abenezer"]
 numbers = [1, 2, 3]
 combo = ["Abel", 1, 2, 3]
 
+#Dictionary
+alphaNum = { 
+    "One": 1,
+    "Two": 2,
+    "Three": 3
+}
+print(alphaNum["Two"])
+
 #print(combo)
 
 #print(numbers[1])
 
-numbers.append(4)
-print(numbers)
-numbers.insert(1, 100)
-print(numbers)
-numbers.extend([300, 400, 25])
-print(numbers)
-numbers.sort()
-print(numbers)
-numbers.reverse()
-print(numbers)
-numbers.remove(400)
-print(numbers)
+#numbers.append(4)
+#print(numbers)
+#numbers.insert(1, 100)
+#print(numbers)
+#numbers.extend([300, 400, 25])
+#print(numbers)
+#numbers.sort()
+#print(numbers)
+#numbers.reverse()
+#print(numbers)
+#numbers.remove(400)
+#print(numbers)
