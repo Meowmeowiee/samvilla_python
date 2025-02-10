64abca207683f9f617698fe7ac9ca117a70ed887
unknown <Meowmeowiee>
2025-02-10 18:30:07 +0800

    initialcondtional

---
+++ conditionals_loops.py
@@ -0,0 +1,4 @@
+age = 23
+
+if age > 20:
+    print("Your are older than 20")
--- variable.py
+++ variable.py
@@ -1,2 +1,8 @@
-name = input("Enter your name: ")
-print("Hello " + name)
+# Ask user for age and tell them their in five years
+age = input("Enter your age: ")
+
+age_in_five_years = int(age) + 5
+
+print("You will be " + str(age_in_five_years) + " years old in five years.")
+
+# 
